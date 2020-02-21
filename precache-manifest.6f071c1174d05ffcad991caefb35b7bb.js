89148cd918f521cc90078d35455e553b7c8d9aa9
Ugbah Isioma <macbook@Isiomas-MacBook-Pro.local>
2020-02-21 09:46:52 +0100

    Updates

---
+++ precache-manifest.6f071c1174d05ffcad991caefb35b7bb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a72401f91bf05734aeb9c2efd17e7b56",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "c4af44b47953e0e3e7cb",
+    "url": "/monsters-rolodex/static/css/main.60f2ef61.chunk.css"
+  },
+  {
+    "revision": "c68e14fdc7e1ba4a5621",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.9e7d0785.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4af44b47953e0e3e7cb",
+    "url": "/monsters-rolodex/static/js/main.d9f7a12d.chunk.js"
+  },
+  {
+    "revision": "9e014af7b12dc10ef867",
+    "url": "/monsters-rolodex/static/js/runtime-main.ff038b7c.js"
+  }
+]);
